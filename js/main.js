98bcbd9151ae2f260ef036e2908b6f24a7da06a7
Gittster <mattsweet147@gmail.com>
2024-07-23 20:52:28 -0500

    Test
    Test

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function showCharacterCreation() {
+    document.getElementById('main-menu').style.display = 'none';
+    document.getElementById('character-creation').style.display = 'block';
+}
+
+function showCharacterSelection() {
+    document.getElementById('main-menu').style.display = 'none';
+    document.getElementById('character-selection').style.display = 'block';
+}
---
+++ js/town.js
@@ -0,0 +1,14 @@
+function goToInventory() {
+    document.getElementById('town').style.display = 'none';
+    document.getElementById('inventory').style.display = 'block';
+}
+
+function goToNPCs() {
+    document.getElementById('town').style.display = 'none';
+    document.getElementById('npcs').style.display = 'block';
+}
+
+function goToZones() {
+    document.getElementById('town').style.display = 'none';
+    document.getElementById('zones').style.display = 'block';
+}
